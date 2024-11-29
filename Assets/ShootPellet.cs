81cf38f46d40636647b01ff68925ec0787e49916
Gryph0123 <gryphninja@gmail.com>
2024-11-29 11:15:21 -0500

    Started the script of the shooting of the pellet

--- Assets/ShootPellet.cs
+++ Assets/ShootPellet.cs
@@ -1,7 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShootPellet : MonoBehaviour
 {
+    public GameObject pellet;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,13 @@ public class ShootPellet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            // Creates the pellet at the ship's position
+            Instantiate(pellet, transform.position, Quaternion.identity);
+
+        }
     }
 }
