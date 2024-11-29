f69f40784d541fb6dc06cb383554f897d122d5b9
Gryph0123 <gryphninja@gmail.com>
2024-11-29 10:19:50 -0500

    Made wrap around and added pellet

--- Assets/Wraparound.cs
+++ Assets/Wraparound.cs
@@ -3,16 +3,39 @@ using UnityEngine;
 public class Wraparound : MonoBehaviour
 {
 
+    // Gets the dimentions of the screen
+    public float borderHeight = Camera.main.orthographicSize;
+    public float left;
+    public float right;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        borderHeight = Camera.main.orthographicSize;
+        float aspect = Camera.main.aspect;
+        left = -borderHeight * aspect;
+        right = borderHeight * aspect;
+        right = borderHeight * aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y > borderHeight)
+        {
+            transform.position = new Vector3(transform.position.x, - borderHeight, 0f);
+        }
+        else if (transform.position.y < -borderHeight)
+        {
+            transform.position = new Vector3(transform.position.x, borderHeight, 0f);
+        }
+        else if (transform.position.x > right)
+        {
+            transform.position = new Vector3(left, transform.position.y, 0f);
+        }
+        else if (transform.position.x < left)
+        {
+            transform.position = new Vector3(right, transform.position.y, 0f);
+        }
     }
 }
